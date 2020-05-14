7d7151a9ba06370b2c5dab56892578d51a0a0793
Okotie <Ottue@mail.ru>
2020-05-14 23:12:00 +0300

    Рефакторинг

--- src/components/SnackbarConst.jsx
+++ src/components/SnackbarConst.jsx
@@ -2,14 +2,6 @@ import React from "react";
 import Alert from "@material-ui/lab/Alert";
 import Snackbar from "@material-ui/core/Snackbar";
 const SnackbarConst = (props) => {
-/*    return(
-        <Snackbar open={props.open} autoHideDuration={props.autoHideDuration} onClose={props.onClose}>
-            <Alert onClose={props.onClose} severity={props.state.severity}>
-                {props.state.text}
-            </Alert>
-        </Snackbar>
-    )*/
-
     return(
         props.stateSnackbar != null ?
             (<Snackbar open={Boolean(props.stateSnackbar)} autoHideDuration={6000} onClose={props.onClose}>
